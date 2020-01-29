679fdf19b1a38fdcc933ad3a4777ae7baef35d49
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2020-01-29 16:28:08 -0300

    updates

--- NET Framework - ASP.NET MVC/WebApplication1/Controllers/HomeController.cs
+++ NET Framework - ASP.NET MVC/WebApplication1/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController(){
+            customers = cache["customers"] as List<Customer>;
+            if(customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
         public ActionResult Index()
         {
             return View();
@@ -16,6 +34,7 @@ namespace WebApplication1.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ViewBag.MySuperProperty = "That is a text by Rodrigo.";
 
             return View();
         }
@@ -25,6 +44,43 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ViewCustomer(string Name, string Telephone)
+        {
+            Customer customer = new Customer();
+
+            customer.Id = Guid.NewGuid().ToString();
+            customer.Name = Name;
+            customer.Telephone = Telephone;
+
+            return View(customer);
+        }
+
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+
+            return RedirectToAction("CustomerList");
+        }
+
+        public ActionResult CustomerList()
+        {
+            //List<Customer> customers = new List<Customer>();
+
+            //customers.Add(new Customer() { Name = "Helena", Telephone = "0215155" });
+            //customers.Add(new Customer() { Name = "Ellenize", Telephone = "111111" });
+
+            return View(customers);
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ASP.NET-MVC
-.NET Framework Model - ASP.NET MVC
+ ASP.NET MVC Template to .NET Framework
+
+ ASP.NET MVC Modelo para .NET FRAMEWORK
+
+##Exemplos
+- Anatomy and an MVC Website;
+- Contollers;
+- Views;
+- Models;
+- Posting Data;
+- Scaffolding - Model Lists;
+- Scaffolding - Posting Models;
+- Scaffolding - Viewing and Editing;
+- Scaffolding - Deleting;
+- Model Validation;
+- Partial Views.
